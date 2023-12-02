2124a7b581a4e06938a0451e300e885d9ce604a0
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2023-12-02 17:39:34 +0100

    edit css

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,15 @@
-const modal = document.querySelector('.backdrop');
-const btnOpen = document.querySelector('.modal-btn-open');
-const btnClose = document.querySelector('.modal-btn-close');
-const toggleModal = () => modal.classList.toggle('is-hidden');
+const body = document.querySelector("body");
+const modal = document.querySelector(".backdrop");
+const btnsOpen = document.querySelectorAll(".modal-btn-open");
+const btnClose = document.querySelector(".modal-btn-close");
 
-btnOpen.addEventListener('click', toggleModal);
-btnClose.addEventListener('click', toggleModal);
+const toggleModal = () => {
+  body.classList.toggle("fixed");
+  modal.classList.toggle("is-hidden");
+};
+
+btnsOpen.forEach((btn) => {
+  btn.addEventListener("click", toggleModal);
+});
+
+btnClose.addEventListener("click", toggleModal);
