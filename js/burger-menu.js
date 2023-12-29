b0b65889c6c98971992bb878d30d41ee1432db80
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2023-12-29 21:26:37 +0100

    edit menu

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,6 +1,7 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
 const menuBtnClose = document.querySelector(".menu-btn-close");
+const menu = document.querySelector(".mobile-menu__nav");
 
 const toggleMenu = () => mobileMenu.classList.toggle("is-open");
 const disableScroll = () =>
@@ -8,6 +9,11 @@ const disableScroll = () =>
 
 menuBtnOpen.addEventListener("click", toggleMenu);
 menuBtnClose.addEventListener("click", toggleMenu);
+
+menu.addEventListener("click", () => {
+  document.body.classList.remove("is-scroll-disabled");
+  mobileMenu.classList.remove("is-open");
+});
 
 menuBtnOpen.addEventListener("click", disableScroll);
 menuBtnClose.addEventListener("click", disableScroll);
